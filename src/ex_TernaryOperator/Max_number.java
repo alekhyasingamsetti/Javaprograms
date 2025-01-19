cdb36798977347fa89e967bdeaf7d5a480d13bf9
alekhyasingamsetti <salekhya12@gmail.com>
2025-01-19 18:46:13 +0530

    first commit 01

---
+++ src/ex_TernaryOperator/Max_number.java
@@ -0,0 +1,11 @@
+package ex_TernaryOperator;
+
+public class Max_number {
+    public static void main(String[] args) {
+        int A = 10;
+        int Y = 20;
+        int max = (A > Y) ? A : Y;
+        System.out.println(max);
+
+    }
+}
