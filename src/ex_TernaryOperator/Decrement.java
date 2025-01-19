6f1748f96b3f21394e09437582cbefe4a83ef7fa
alekhyasingamsetti <salekhya12@gmail.com>
2025-01-19 18:57:13 +0530

    first commit 01

---
+++ src/ex_TernaryOperator/Decrement.java
@@ -0,0 +1,9 @@
+package ex_TernaryOperator;
+
+public class Decrement {
+    public static void main(String[] args){
+        int a = 20;
+        System.out.println(--a + a++ + a--);
+        System.out.println(a);
+    }
+}
---
+++ src/ex_TernaryOperator/Increment.java
@@ -0,0 +1,11 @@
+package ex_TernaryOperator;
+
+public class Increment {
+    public static void main(String[] args){
+        int a = 10;
+
+        System.out.println(++a + a++ + a++);
+
+        System.out.println(a);
+    }
+}
